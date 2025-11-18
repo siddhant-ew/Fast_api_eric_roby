959d309bf3f56b0a7184705ad4546d6c4f377657
Siddhant Kadiyal <siddhantk@digisignals.com>
2025-11-18 18:10:34 +0530

    added the @abstraction class explaination

--- oops3/abstraction.py
+++ oops3/abstraction.py
@@ -1,5 +1,12 @@
 from abc import ABC, abstractmethod
 
+# ABC = Abstract Base Class
+
+# @abstractmethod is a decorator in Python that you use inside an abstract class to say,
+# “Every child class must create its own version of this method.” The abstract class only gives the rule or idea, 
+# but doesn’t provide the actual code for the method. Because it’s just a blueprint, you cannot create an object of an abstract class. 
+# Only the child classes that implement all the abstract methods can be used to make objects. This helps ensure that all child classes follow the same structure.
+
 class gadi(ABC):
     @abstractmethod
 
@@ -8,7 +15,7 @@ class gadi(ABC):
         pass
 
     def off_engine(self):
-        print("engine has beed turned off")
+        print("engine has been turned off")
         pass
 
 class car(gadi):
@@ -30,3 +37,23 @@ vehicles = [car(), truck()]
 for v in vehicles:
     v.start_engine()
     v.off_engine()
+
+
+# a = gadi() ---we cannot use the abstract class for the object making, the type error will throw like this - 
+#               TypeError: Can't instantiate abstract class gadi without an implementation for abstract method 'start_engine'
+
+# Real Reason Why Abstraction Is Important (Practical Explanation):
+    # Abstraction is important because it helps you design clean, predictable, and error-free software, 
+    # especially when multiple developers or multiple types of objects are involved.
+    # Think of abstraction as creating a contract or mandatory rule-set that all subclasses must follow.
+
+# Imagine you're building a software system with 10 engineers.
+# If everyone writes methods however they want, your project will become a mess.
+
+# Example problem without abstraction:
+# One developer names method start().
+# Another names it start_engine().
+# Another names it turnOn().
+
+# Now your main program cannot depend on any consistent method name.
+# Abstraction fixes this.
