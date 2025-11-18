4bdc0c28b4a4bc84c99ba29cfd531908f1ff7e08
Siddhant Kadiyal <siddhantk@digisignals.com>
2025-11-18 18:19:28 +0530

    added the explaination for the encapsulation

--- oops3/encapsulation1.py
+++ oops3/encapsulation1.py
@@ -7,3 +7,12 @@ print(add_money)
 
 acc.withdraw(1)
 acc.get_balance()
+
+
+# Encapsulation = Locking things + giving safe doors to use them
+# Just like:
+# You don’t touch the inside of a computer, you use buttons
+# You don’t touch the engine of a car, you use the steering and pedals
+# You don’t touch the bank vault, you withdraw using ATM
+
+# Same in code!
